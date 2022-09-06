b605f2fde0347d282eefdb2badae4693a2583550
Lidia Kovac <lidia.kovac1998@gmail.com>
2022-09-07 00:22:13 +0200

    aggiunge easter egg

--- src/components/New/NewVersion.jsx
+++ src/components/New/NewVersion.jsx
@@ -5,10 +5,11 @@ import { Accordion } from "react-bootstrap";
 export const NewVersion = () => {
   return (
     <div className="accordion__wrap">
-      <h2>News!</h2>
+      <h2>News e memini!</h2>
       <small>
         Raga non so come si numerano le versioni. Se il numero sale vuol dire che sta andando tutto
-        bene.
+        bene. <br/>
+        Sappiate che questa sezione e' incredibilmente ostile. Senza motivo, mi sento acida stasera.
       </small>
       <Accordion>
         <Accordion.Item eventKey="0">
@@ -24,6 +25,26 @@ export const NewVersion = () => {
               </li>
               <li>
                 Per la gioia di Mauro, ora potete cambiare domanda anche cliccando sulla domanda
+              </li>
+            </ul>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Ringraziamenti e note</Accordion.Header>
+          <Accordion.Body className='accordion-ringraziamenti'>
+            <small>Si, come nella tesi. Tanto se continuo a sviluppare stronzate invece che studiare non mi laureo mica.</small>
+            <ul>
+              <li>
+                Vee, Mai e Carlotta: domande incredibilmente imbarazzanti e rovina-relazioni
+              </li>
+              <li>
+                Jack: continua a cercare di rompere le mie app, magari un giorno ce la fai, coglione.
+              </li>
+              <li>
+                Fabri: il tuo parkinson mi ha permesso di scoprire un bug. 
+              </li>
+              <li>
+                Riky: Hai pensato che avrei creato un backend per questa cosa. Un backend con Docker. Confidence boost fondamentale.
               </li>
             </ul>
           </Accordion.Body>
